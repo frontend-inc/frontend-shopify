c0af1fc1a149f68b4fe891c86d4aa4127b3dc992
Rami Bitar <rbitar@gmail.com>
2024-01-14 13:21:16 -0500

    Export constants

--- dist/client/index.d.ts
+++ dist/client/index.d.ts
@@ -2,3 +2,5 @@ export { createClient } from './ShopifyClient';
 export { useApollo } from './apollo';
 export { createApolloClient } from './apollo';
 export { getShopifyIdFromGid } from './utils';
+export { PRODUCT_SORT_OPTIONS } from './constants';
+export { COLLECTION_SORT_OPTIONS } from './constants';
