e24ab8b7efa791438785d1b5bba60462b5da44ae
Rami Bitar <rbitar@gmail.com>
2024-01-14 13:02:17 -0500

    Update exports

--- dist/client/index.d.ts
+++ dist/client/index.d.ts
@@ -1,3 +1,5 @@
 export { createClient } from './ShopifyClient';
 export { useApollo } from './apollo';
 export { createApolloClient } from './apollo';
+export * from './types';
+export * from './utils';
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,2 @@
 export * from './client';
 export * from './graphql';
-export * from './client/types';
-export * from './client/utils';
